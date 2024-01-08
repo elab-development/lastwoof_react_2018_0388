c0fa1f28853911d59d1e3bc147740e869caf2873
Andrea Tambur <andrea.tambur@gmail.com>
2024-01-08 14:35:12 +0100

    O nama

--- src/stranice/ONama.jsx
+++ src/stranice/ONama.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
+import andrea from '../slike/andrea.jpg';
+import ivana from '../slike/ivana.jpg';
+import {Col, Row, Image} from "react-bootstrap";
 
-const ONama = () => {
+
+const ONama = () => { 
+
+    const ONama=[
+
+        {
+            id:1,
+            imeprezime: "Ivana Milidragovic",
+            brojgodina: "23",
+            opis:"Student Fakulteta organizacionih nauka u Beogradu.",
+            slika: ivana
+        },
+
+        {
+            id:2,
+            imeprezime: "Andrea Tambur",
+            brojgodina: "24",
+            opis: "Student Fakulteta organizacionih nauka u Beogradu.",
+            slika: andrea
+
+        }
+
+    ];
+
     return (
-        <div>
-            O Nama
-        </div>
+       <>
+        <div className="header">
+                <h1>O nama</h1>
+            </div>
+       <Row>
+
+       {
+                    ONama.map(mi => (
+                        <Col key={mi.id}>
+                            
+                            <h1>
+                                {
+                                    mi.imeprezime
+                                
+                                }
+                            </h1>
+
+                            <p>
+                                {
+                                    mi.brojgodina
+                                } godine
+                            </p>
+
+                            <Image widht={600} height={400}  src={mi.slika}>
+
+                            </Image>
+                            
+                            <p>
+                                {
+                                    mi.opis
+                                }
+                            </p>
+                            
+                             </Col>
+                    ))
+                }
+
+       </Row>
+       </>
     );
 };
 
