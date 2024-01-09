d0cb0e05d8dae01fdc0db53204f969dfd1b5f41e
Andrea Tambur <andrea.tambur@gmail.com>
2024-01-09 13:29:22 +0100

    Usluge

--- src/stranice/Usluge.jsx
+++ src/stranice/Usluge.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import {Bs1CircleFill, Bs2CircleFill, Bs3CircleFill, Bs4CircleFill, Bs5CircleFill, Bs6CircleFill} from "react-icons/bs";
+import {Col, Row} from "react-bootstrap";
+import JednaUsluga from "../komponente/JednaUsluga";
 
 const Usluge = () => {
+
+    const usluge = [
+        {
+            id: 1,
+            naziv: "Sahrana",
+            cena: 5000,
+            icon: <Bs1CircleFill />,
+            boja: "danger"
+        },
+        {
+            id: 2,
+            naziv: "Kremacija",
+            cena: 10000,
+            icon: <Bs2CircleFill />,
+            boja: "warning"
+        },
+        {
+            id: 3,
+            naziv: "Eutanazija",
+            cena: 20000,
+            icon: <Bs3CircleFill />,
+            boja: "light"
+        },
+        {
+            id: 4,
+            naziv: "Kovceg",
+            cena: 8000,
+            icon: <Bs4CircleFill />,
+            boja: "secondary"
+        },
+        {
+            id: 5,
+            naziv: "Urma",
+            cena: 9000,
+            icon: <Bs5CircleFill />,
+            boja: "primary"
+        },
+        {
+            id: 6,
+            naziv: "Umrlica",
+            cena: 3000,
+            icon: <Bs6CircleFill />,
+            boja: "dark"
+        },
+        ];
+
     return (
-        <div>
-            Usluge
-        </div>
+        <>
+            <div className="header">
+                <h1>Usluge</h1>
+                <p>Usluge koje pruzamo </p>
+            </div>
+            <Row className="mt-3">
+                {
+                    usluge.map(usluga => (
+                        <Col key={usluga.id}> <JednaUsluga naziv={usluga.naziv} cena={usluga.cena} icon={usluga.icon} boja={usluga.boja} /> </Col>
+                    ))
+                }
+            </Row>
+        </>
     );
 };
 
